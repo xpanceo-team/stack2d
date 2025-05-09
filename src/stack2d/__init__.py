9fe463019d991b43845d371c2010c60f88bebdf8
Arslan Mazitov <arslan.mazitov@phystech.edu>
2025-05-09 23:12:12 +0200

    Linting fix

--- src/stack2d/__init__.py
+++ src/stack2d/__init__.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from .generator import HeterostructureGenerator
 
+__all__ = ["HeterostructureGenerator"]
 PACKAGE_ROOT = Path(__file__).parent.resolve()
 
 __version__ = "0.0.0"
